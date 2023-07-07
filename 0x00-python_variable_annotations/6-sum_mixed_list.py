a229c2fefcdf232bbf7119d5cdb8fcc7950125b1
sethdanny <naddulidaniel1994@gmail.com>
2023-07-07 15:08:39 +0300

    use union

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 '''Module of sum of mixed list of list items'''
 
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """sum of mixed numbers"""
 
     return float(sum(mxd_lst))
